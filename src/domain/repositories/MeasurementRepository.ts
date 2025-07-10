01bc520b0339c3e7d46ac4dd6a548049abec5964
Deivis Olascoaga <deivisalcedo.o@gmail.com>
2025-07-09 19:36:24 -0500

    creacacion del caso de medidas

--- src/domain/repositories/MeasurementRepository.ts
+++ src/domain/repositories/MeasurementRepository.ts
@@ -2,10 +2,10 @@ import type { Measurement } from "../entities/Measurement";
 
 export interface MeasurementRepository {
   getMeasurements(): Promise<Measurement[]>;
-  getMeasurementById(id: number): Promise<Measurement>;
-  getMeasurementByClientId(id: number): Promise<Measurement>;
-  getAreaTotal(id: number): Promise<Measurement>;
+  getMeasurementById(Measurement_ID: number): Promise<Measurement>;
+  getMeasurementByClientId(Client_ID: number): Promise<Measurement>;
+  getAreaTotal(Measurement_ID: number): Promise<Measurement>;
   createMeasurement(Measurement: Measurement): Promise<Measurement>;
   updateMeasurement(Measurement: Measurement): Promise<Measurement>;
-  deleteMeasurement(id: number): Promise<number>;
+  deleteMeasurement(Measurement_ID: number): Promise<number>;
 }
