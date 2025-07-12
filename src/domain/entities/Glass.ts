a9934acc4b8c943e218d83ee9a945ef7d7434ce3
Deivis Olascoaga <deivisalcedo.o@gmail.com>
2025-07-11 21:47:45 -0500

    agregando atributos a glass

--- src/domain/entities/Glass.ts
+++ src/domain/entities/Glass.ts
@@ -1,6 +1,10 @@
 export interface Glass {
-  id?: number,
-  ancho_cm: number,
-  alto_cm: number,
-  artefactoId: number
+  id: number;
+  ancho_cm: number;
+  alto_cm: number;
+  espesor: number;
+  precioM2: number;
+  color: string;
+  tipo: string;
+  artefactoId: number;
 }
